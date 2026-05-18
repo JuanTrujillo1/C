a6591f822eb7789921374a3ed2e5d73300393460
JTrujillo <trujillosolera@gmail.com>
2026-05-18 15:49:17 -0500

    se agregaron los condicionales

--- prueba.c
+++ prueba.c
@@ -23,21 +23,40 @@ void main(){
     printf("Ingrese un el segundo valor: ");
     scanf("%f",&b);
 
-    printf("\nLa suma de los dos valores es: %.2f\n", a+b);
-    printf("La resta de los dos valores es: %.2f\n", a-b);
-    printf("La multiplicacion de los dos valores es: %.2f\n", a*b);
+    c= a+b;
+
+    printf("\nLa suma de los dos valores es: %.2f\n", c);
+
+    c = a-b;
+
+    printf("La resta de los dos valores es: %.2f\n", c);
+
+    c= a*b;
+
+    printf("La multiplicacion de los dos valores es: %.2f\n",c);
 
     c = a / b;
 
     printf("La division de los dos valores es: %.2f\n", c);
 
-    c = pow(a, b);
+    c = (float)pow((double)a, (double)b);
+    /*se usa el pow de esta forma ya que corrompe los datos de a y b*/
 
     printf("La potencia de los dos valores es: %.2f\n", c);
 
     c = fmod(a, b);
     
     printf("El residuo de los dos valores es: %.2f\n", c);
+
+    if (a < b){
+        printf("%.2f es MENOR que %.2f\n",a,b);
+
+    }else if (a > b){
+        printf("%.2f es MAYOR que %.2f\n",a,b);
+
+    }else{
+        printf("%.2f es IGUAL que %.2f\n",a,b);
+    }
 
 }
 
